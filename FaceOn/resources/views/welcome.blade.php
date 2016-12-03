cf44dadc1bfdae6263e66e435dd0f7a38ac0bb00
Angela Adams <aadams3867@gmail.com>
2016-12-02 19:27:13 -0500

    Slightly tweak welcome.blade.php

--- FaceOn/resources/views/welcome.blade.php
+++ FaceOn/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                FaceOn
+                {{ config('app.name') }}
             </div>
 
             <div class="links">
