943bfcf04bb67f6510f929f0bc881d4605e04e17
Angela Adams <aadams3867@gmail.com>
2016-12-02 19:11:53 -0500

    Commit initial Laravel project files

---
+++ FaceOn/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                FaceOn
+            </div>
+
+            <div class="links">
+                <a href="https://laravel.com/docs">Documentation</a>
+                <a href="https://laracasts.com">Laracasts</a>
+                <a href="https://laravel-news.com">News</a>
+                <a href="https://forge.laravel.com">Forge</a>
+                <a href="https://github.com/laravel/laravel">GitHub</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
