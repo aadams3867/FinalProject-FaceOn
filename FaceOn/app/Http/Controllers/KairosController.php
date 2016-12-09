356691edcbe97a39bb02614c836504fbc5c5ee5b
Angela Adams <aadams3867@gmail.com>
2016-12-09 00:30:23 -0500

    Save rudimentary getObject function for later use

--- FaceOn/app/Http/Controllers/KairosController.php
+++ FaceOn/app/Http/Controllers/KairosController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class KairosController extends Controller
 {
-    //
+    /**
+     * Download image file from Amazon S3.
+     *
+     * @param $data Image filename
+     * @return Response
+     */
+    public function downloadFileFromS3($data)
+    {
+        // Send a GetObject request
+        $s3->getObject([
+            'Bucket' => 'face-on-bucket',
+            'Key'    => 'AKIAJGVM46L2RHCVU2NA',  // FIX
+/*            'SourceFile'   => $data,*/
+            'SaveAs'   => 'C:\Users\angsu\Desktop', // The path to a file to save the obj data - FIX
+        ]);
+
+
+/*echo $halfURL;
+?><br><br><?php
+echo $url;
+?><br><br><?php
+var_dump($data);
+die;*/
+
+    }
 }
+
+
+
